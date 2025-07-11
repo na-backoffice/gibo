00016ba2180d105bdedfdcf0aad1cc151bf5d7fc
NA Backoffice <backoffice@needful-apps.de>
2025-07-11 10:57:46 +0200

    eingedeutscht

--- greeter.py
+++ greeter.py
@@ -1,16 +1,16 @@
 import sys
 from datetime import datetime
 
-def greet(name="Stranger"):
-    hour = datetime.now().hour
-    if 5 <= hour < 12:
-        part = "morning"
-    elif 12 <= hour < 18:
-        part = "afternoon"
+def begruessen(name="Fremder"):
+    stunde = datetime.now().hour
+    if 5 <= stunde < 12:
+        teil = "Morgen"
+    elif 12 <= stunde < 18:
+        teil = "Nachmittag"
     else:
-        part = "evening"
-    print(f"\033[1;36mGood {part}, {name}! Welcome to your command line.\033[0m")
+        teil = "Abend"
+    print(f"\033[1;36mGuten {teil}, {name}! Willkommen in deiner Kommandozeile.\033[0m")
 
 if __name__ == "__main__":
     name = sys.argv[1] if len(sys.argv) > 1 else None
-    greet(name)
+    begruessen(name)
